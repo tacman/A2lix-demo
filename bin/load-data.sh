b432d0f5386f0050652642080211de75cd4770b8
Tac Tacelosky <tacman@gmail.com>
2022-08-25 13:48:17 -0400

    initial version

---
+++ bin/load-data.sh
@@ -0,0 +1,10 @@
+dbname=a2lix
+
+#echo "create user main WITH ENCRYPTED PASSWORD 'main';" | sudo -u postgres psql
+
+echo "drop database if exists $dbname; create database $dbname; grant all privileges on database $dbname to main; " | sudo -u postgres psql
+#echo "CREATE EXTENSION pg_trgm;" | sudo -u postgres psql -d $dbname
+#echo "CREATE EXTENSION hstore;" | sudo -u postgres psql -d $dbname
+echo "database $dbname now ready for migrations or restore"
+
+bin/console doctrine:schema:update --force
